1aac13ef8224bb3e247d5313a9d46e0a3ac79bd6
Timur Girgin <girgin.timur@gmail.com>
2021-03-04 16:30:26 -0500

    fix(center): element is centered at all times, no matter where on the page one is

--- src/components/VStep.vue
+++ src/components/VStep.vue
@@ -206,7 +206,7 @@ export default {
 
     &__center {
       width: 50%;
-      position: absolute;
+      position: fixed;
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
