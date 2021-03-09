e92c6a3a909638a9c1f29b6ded4e8beed62125cd
Timur Girgin <girgin.timur@gmail.com>
2021-03-08 21:23:51 -0500

    test(v-step): add test to check for presence of v-mask component when setting mask param to true

--- tests/unit/VStep.spec.js
+++ tests/unit/VStep.spec.js
@@ -1,5 +1,6 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import VStep from '@/components/VStep.vue'
+import VMask from '@/components/VMask.vue'
 import { DEFAULT_OPTIONS, DEFAULT_STEP_OPTIONS } from '@/shared/constants'
 import { customAlphabet } from 'nanoid'
 
@@ -21,6 +22,31 @@ describe('VStep.vue', () => {
     })
 
     expect(wrapper.element).toMatchSnapshot()
+
+    expect(wrapper.text()).toContain(step.content)
+  })
+  it('renders with a mask', () => {
+    const step = {
+      target: 'v-step-0',
+      content: 'This is a demo step!',
+      params: {
+        mask: true
+      }
+    }
+
+    const wrapper = mount(VStep, {
+      propsData: {
+        step,
+        stepHash: customAlphabet('1234567890abcdef', 8)(),
+        stepParams: DEFAULT_STEP_OPTIONS,
+        stop: () => {},
+        labels: DEFAULT_OPTIONS.labels
+      }
+    })
+
+    // expect(wrapper.element).toMatchSnapshot()
+    expect(wrapper.findComponent(VMask))
+    console.log(wrapper.findComponent(VMask))
 
     expect(wrapper.text()).toContain(step.content)
   })
